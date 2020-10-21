9353f04cdc49f6cbdba940231c7cade8e4627da5
AceWealth <72768999+AceWealth@users.noreply.github.com>
2020-10-21 16:05:45 -0400

    Update NavBar.js

--- src/logged_out/components/navigation/NavBar.js
+++ src/logged_out/components/navigation/NavBar.js
@@ -82,7 +82,7 @@ function NavBar(props) {
               display="inline"
               color="primary"
             >
-              Wa
+              Liv
             </Typography>
             <Typography
               variant="h4"
@@ -90,7 +90,7 @@ function NavBar(props) {
               display="inline"
               color="secondary"
             >
-              Ver
+              Wealth
             </Typography>
           </div>
           <div>
